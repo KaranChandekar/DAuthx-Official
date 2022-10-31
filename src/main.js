8cb2f6bf77038431ea849437a5ce27de359f7313
Karan <karanchandekar431@gmail.com>
2022-10-31 22:01:00 +0530

    created and styled coming soon bar

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Navbar from "./Components/Navbar/Navbar";
 import Section1 from "./Components/Section1/Section1";
+import ComingSoonBar from "./Components/Coming Soon Bar/ComingSoonBar";
 import Features from "./Components/Features/Features";
 import Section2 from "./Components/Section2/Section2";
 import Footer from "./Components/Footer/Footer";
@@ -9,6 +10,7 @@ function Main() {
     <div className="main">
       <Navbar />
       <Section1 />
+      <ComingSoonBar />
       <Features />
       <Section2 />
       <Footer />
