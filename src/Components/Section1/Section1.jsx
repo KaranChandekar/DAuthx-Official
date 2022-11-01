2d0e096ef404faf6ed2609322ded883adb5dfcb7
Karan <karanchandekar431@gmail.com>
2022-11-01 20:39:07 +0530

    update section 1

--- src/Components/Section1/Section1.jsx
+++ src/Components/Section1/Section1.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Section1.css";
+import Navbar from "../Navbar/Navbar";
+import ComingSoonBar from "../Coming Soon Bar/ComingSoonBar";
 import arrow from "../Images/arrow.png";
 import whatsapp from "../Images/whatsapp-logo.png";
 import discord from "../Images/discord-logo.png";
@@ -12,41 +14,46 @@ import polkadot from "../Images/polkadot-logo.png";
 import cosmos from "../Images/cosmos-atom-logo.png";
 import tron from "../Images/tron-logo.png";
 import bnb from "../Images/bnb-logo.png";
+import Main from "../../main";
 
 const Section1 = () => {
   return (
-    <div className="section-1">
-      <div className="text">
-        <h1>
-          <span>On-Chain</span>
-          <br /> Identity Bridge
-        </h1>
-        <p>
-          Smart Contract protocol for decentralised Authentication of Web2 Id's
-          and user account on Blockchain.
-        </p>
-        <div className="btn-wrapper">
-          <button className="btn btn-left">Get Started</button>
-          <button className="btn btn-right">How it Works?</button>
+    <div className="section-1-main">
+      <Navbar />
+      <div className="section-1">
+        <div className="text">
+          <h1>
+            <span>On-Chain</span>
+            <br /> Identity Bridge
+          </h1>
+          <p>
+            Smart Contract protocol for decentralised Authentication of Web2
+            Id's and user account on Blockchain.
+          </p>
+          <div className="btn-wrapper">
+            <button className="btn btn-left">Get Started</button>
+            <button className="btn btn-right">How it Works?</button>
+          </div>
+        </div>
+        <div className="img-container">
+          <div className="img-wrapper">
+            <img src={whatsapp} alt="whatsapp" />
+            <img src={discord} alt="discord" />
+            <img src={twitter} alt="twitter" />
+            <img src={gmail} alt="gmail" />
+          </div>
+          <div className="img-wrapper arrow-wrapper">
+            <img src={arrow} alt="arrow" className="arrow-img" />
+          </div>
+          <div className="img-wrapper">
+            <img src={polkadot} alt="polkadot" />
+            <img src={solana} alt="solana" />
+            <img src={polygon} alt="polygon" />
+            <img src={ethereum} alt="ethereum" />
+          </div>
         </div>
       </div>
-      <div className="img-container">
-        <div className="img-wrapper">
-          <img src={whatsapp} alt="whatsapp" />
-          <img src={discord} alt="discord" />
-          <img src={twitter} alt="twitter" />
-          <img src={gmail} alt="gmail" />
-        </div>
-        <div className="img-wrapper arrow-wrapper">
-          <img src={arrow} alt="arrow" className="arrow-img" />
-        </div>
-        <div className="img-wrapper">
-          <img src={polkadot} alt="polkadot" />
-          <img src={solana} alt="solana" />
-          <img src={polygon} alt="polygon" />
-          <img src={ethereum} alt="ethereum" />
-        </div>
-      </div>
+      <ComingSoonBar />
     </div>
   );
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
-import Navbar from "./Components/Navbar/Navbar";
 import Section1 from "./Components/Section1/Section1";
-import ComingSoonBar from "./Components/Coming Soon Bar/ComingSoonBar";
 import Section2 from "./Components/Section2/Section2";
 import Section3 from "./Components/Section3/Section3";
 import Section4 from "./Components/Section4/Section4";
@@ -9,13 +7,10 @@ import Footer from "./Components/Footer/Footer";
 function Main() {
   return (
     <div className="main">
-      <Navbar />
       <Section1 />
-      <ComingSoonBar />
       <Section2 />
       <Section3 />
       <Section4 />
-      <Footer />
     </div>
   );
 }
