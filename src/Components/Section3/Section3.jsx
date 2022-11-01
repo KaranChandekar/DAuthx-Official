79e7817e04d7c7d2f2f622a1d9840848b4334d06
Karan <karanchandekar431@gmail.com>
2022-11-01 16:59:40 +0530

    update section styles

--- src/Components/Section3/Section3.jsx
+++ src/Components/Section3/Section3.jsx
@@ -11,21 +11,20 @@ const Section3 = () => {
     "fa-solid fa-coins",
   ];
   const text = [
-    "Phone number / email based 2 factor authentication on-chain",
-    "Smart contact authorisation vai OTP instead of private key",
-    "Easy recovery of tokens and NFts from staking smart contracts",
-    "User can link web3 wallet with any web2 id",
-    "On-Chain authenticated web2 social Ids",
+    "Phone number / email based 2 factor authentication on-chain.",
+    "Smart contact authorisation vai OTP instead of private key.",
+    "Easy recovery of tokens and NFts from staking smart contracts.",
+    "User can link web3 wallet with any web2 id.",
+    "On-Chain authenticated web2 social Ids.",
   ];
   return (
     <div className="section-3">
-      <h1 className="heading">Features & Use Cases</h1>
+      <h1 className="heading">Use Cases</h1>
       <div className="features-container">
         <Feature icon={icon[0]} text={text[0]} />
         <Feature icon={icon[1]} text={text[1]} />
         <Feature icon={icon[2]} text={text[2]} />
         <Feature icon={icon[3]} text={text[3]} />
-        <Feature icon={icon[4]} text={text[4]} />
       </div>
     </div>
   );
